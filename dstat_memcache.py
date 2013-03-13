c0487ad46d10e8e347ba4ef21c1127e50d98f321
Neil Gentleman <neil@pulseenergy.com>
2013-03-13 12:14:15 -0700

    show rate, not total

---
+++ dstat_memcache.py
@@ -0,0 +1,31 @@
+### Author: Dean Wilson <dean.wilson@gmail.com>
+
+class dstat_plugin(dstat):
+    """
+    Memcache hit count plugin.
+
+    Displays the number of memcache get_hits and get_misses.
+    """
+    def __init__(self):
+        self.name = 'memcache'
+        self.nick = ('hit', 'miss', 'evict', )
+        self.vars = ('get_hits', 'get_misses', 'evictions')
+        self.type = 'd'
+        self.width = 6
+        self.scale = 50
+
+    def check(self):
+        try:
+            global memcache
+            import memcache
+            self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        except:
+            raise Exception, 'Plugin needs the memcache module'
+
+    def extract(self):
+        stats = self.mc.get_stats()
+        for name in self.vars:
+            self.set2[name] = long(stats[0][1][name])
+            self.val[name] = (self.set2[name] - self.set1[name]) * 1.0 / elapsed
+        if step == op.delay:
+            self.set1.update(self.set2)
